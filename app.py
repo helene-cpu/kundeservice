1d5647cf81ecac8d1562df7b1bc161285d5ad358
helene-cpu <66snahel@stud.akademiet.no>
2026-02-27 12:43:49 +0000

    Lagt til ny route til å sende inn sak

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask (__name__)
 def index():
     return render_template ("index.html")
 
+@app.route('/send')
+def send():
+    return render_template("send.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
